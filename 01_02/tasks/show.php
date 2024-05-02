fb8e2849c24032e6b45c62a1c3d5ba5cd01c5bfe
Donovan <32802098+syevers@users.noreply.github.com>
2024-05-02 02:02:29 +0000

    Pending changes exported from your codespace

--- 01_02/tasks/show.php
+++ 01_02/tasks/show.php
@@ -1,12 +1,20 @@
 <?php
-
 // hostname: "127.0.0.1"
 // username: "mariadb"
 // password: "mariadb"
 // database: "mariadb"
 // port:     3306
-
+$db = mysqli_connect('127.0.01', 'mariadb', 'mariadb', 'mariadb', 3306);
+$query = 'SELECT * FROM tasks';
+$result = mysqli_query($db, $query);
+$task =  mysqli_fetch_assoc($result);
+// echo print_r($task['id']);
+// echo json_encode($task);
+// foreach ($result as $row) {
+//   echo print_r($row);
+// }
 ?>
+
 
 <!doctype html>
 <html lang="en">
@@ -25,19 +33,19 @@
 
       <dl>
         <dt>ID</dt>
-        <dd></dd>
+        <dd><?php echo $task['id']; ?> </dd>
       </dl>
       <dl>
         <dt>Priority</dt>
-        <dd></dd>
+        <dd><?php echo $task['priority']; ?></dd>
       </dl>
       <dl>
         <dt>Completed</dt>
-        <dd></dd>
+        <dd><?php echo $task['completed'] == true; ?></dd>
       </dl>
       <dl>
         <dt>Description</dt>
-        <dd></dd>
+        <dd><?php echo $task['description'];?></dd>
       </dl>
 
     </section>
--- 01_04/tasks/index.php
+++ 01_04/tasks/index.php
@@ -1,47 +1,69 @@
 <?php
 
-// hostname: "127.0.0.1"
-// username: "mariadb"
-// password: "mariadb"
-// database: "mariadb"
-// port:     3306
+$hostname = "127.0.0.1";
+$username = "mariadb";
+$password = "mariadb";
+$database = "mariadb";
+$port =     3306;
 
+//  Test if connection succeeded
+$db = mysqli_connect($hostname, $username, $password, $database);
+if (mysqli_errno($db)) {
+  $msg = 'failed to connect to database';
+  $msg .= mysqli_connect_error();
+  $msg .= " ( " . mysqli_connect_errno() . " ) ";
+  exit($msg);
+}
+
+$sql_query = 'SELECT * FROM tasks';
+$result = mysqli_query($db, $sql_query);
+$id = mysqli_fetch_assoc($result);
+echo ($id['id']);
+foreach() {
+
+}
 ?>
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>Task Manager: Task List</title>
-  </head>
-  <body>
 
-    <header>
-      <h1>Task Manager</h1>
-    </header>
+<head>
+  <title>Task Manager: Task List</title>
+</head>
 
-    <section>
+<body>
 
-      <h1>Task List</h1>
+  <header>
+    <button>irsenteisnt</button>
+    <h1>Task Manager</h1>
+  </header>
 
-    	<table>
-    	  <tr>
-          <th>ID</th>
-          <th>Priority</th>
-          <th>Completed</th>
-    	    <th>Description</th>
-    	  </tr>
+  <section>
 
-        <?php // loop through tasks ?>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-      	    <td></td>
-      	  </tr>
-        <?php // end loop ?>
-    	</table>
+    <h1>Task List</h1>
 
-    </section>
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>Priority</th>
+        <th>Completed</th>
+        <th>Description</th>
+      </tr>
 
-  </body>
+      <?php // loop through tasks 
+      ?>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <?php // end loop 
+      ?>
+    </table>
+
+  </section>
+
+</body>
+
 </html>
